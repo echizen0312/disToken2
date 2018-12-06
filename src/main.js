0042a830c444d38f0aeea908d35b7581f26a8aa5
Izayoi Sakuya <sakuya0312@qq.com>
2018-12-06 16:35:15 +0800

    0.1.0 初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,29 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import 'material-design-icons/iconfont/material-icons.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import Message from 'muse-ui-message'
+import 'muse-ui-message/dist/muse-ui-message.css'
+import Loading from 'muse-ui-loading'
+import 'muse-ui-loading/dist/muse-ui-loading.css'
+import VueCookies from 'vue-cookies'
+import VueResource from 'vue-resource'
+import VueClipboard from 'vue-clipboard2'
+
+Vue.use(MuseUI)
+Vue.use(Message)
+Vue.use(Loading)
+Vue.use(VueCookies)
+Vue.use(VueResource)
+Vue.use(VueClipboard)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h = > h(App)
+}).
+$mount('#app')
